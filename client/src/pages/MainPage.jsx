065416774e51fe3a40eb038a5255a8156902f6f5
Timothy Dennis <timothyddennis@gmail.com>
2024-02-04 10:55:47 -0600

    fix: edited main page to handle api call for retrieving all recipes from db

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function MainPage() {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const getRecipes = async () => {
+            const recipes = await axios.get(`/api/recipes`);
+            setRecipes(recipes.data);
+        }
+        getRecipes();
+    }, [])
+
     return (
         <div>
             <h2>All Recipes</h2>
+            <div>
+            {recipes.map((recipe, x) => (
+                <div className="recipe-card" key={x}>
+                    {console.log(recipe)}
+                    <p>{recipe.name}</p>
+                    <p>{recipe.description}</p>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
