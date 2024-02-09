0953ddfad96720b6a56aac0facbdf5cf4d57115b
Timothy Dennis <timothyddennis@gmail.com>
2024-02-09 07:38:56 -0600

    feat: added button to show or hide type filters on front page

--- client/src/forms/FindByFilterForm.jsx
+++ client/src/forms/FindByFilterForm.jsx
@@ -1,8 +1,37 @@
 import RecipeType from "./formComponents/RecipeType";
+import { useState, useEffect } from "react";
 import SubmitButton from "./formComponents/SubmitButton";
 import "../stylesheets/forms.css";
+import { isMobile } from "react-device-detect";
+
 
  function FindByTypeForm({handleSubmit, getRecipes}) {
+
+    const [openState, setOpenState] = useState(false);
+    const [menuState, setMenuState] = useState(<></>)
+
+    const handleOpenClick = () => {
+        if (!openState) {
+            setOpenState(true)
+        } else if (openState) {
+            setOpenState(false)
+        }
+    }
+
+    useEffect(() => {
+        if (openState) {
+            setMenuState(
+            <div>
+                <form id="find-recipe-types-form" className="type-form" method="POST" onSubmit={handleSubmit} action="/api/recipes">
+                <RecipeType />
+                <SubmitButton />
+                </form>
+                <button id="reset-filters-btn" className="form-field-input" onClick={resetFilters}>Reset Filters</button>
+            </div>)
+        } else if (!openState) {
+            setMenuState(<></>)
+        }
+    }, [openState])
 
     const resetFilters = () => {
         document.querySelectorAll("input[type='checkbox']:checked").forEach(element => element.click());
@@ -13,12 +42,11 @@ import "../stylesheets/forms.css";
 
     return (
         <div id="find-recipe-types">
-            <form id="find-recipe-types-form" className="type-form" method="POST" onSubmit={handleSubmit} action="/api/recipes">
-                <RecipeType />
-                <SubmitButton />
-            </form>
-            <button id="reset-filters-btn" className="form-field-input"  onClick={resetFilters}>Reset Filters</button>
-        </div>
+            <div>
+            <button onClick={handleOpenClick}></button>
+            {menuState}                
+            </div>
+          </div>
     )
 }
 
